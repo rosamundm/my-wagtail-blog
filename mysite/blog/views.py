2922a30d82ed1b69f7a2a8ce02bb861f230eb0e5
rosamund <rsmnd.mthr@gmail.com>
2019-10-26 15:54:08 +0200

    first commit

---
+++ mysite/blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+#def post_list(request):
+#    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#    return render(request, 'blog/blog_index_page.html', {'posts': posts})
