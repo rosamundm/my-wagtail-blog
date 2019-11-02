c42300014fbb1fe8b1227d1a3a351f61f17c38be
rosamund <rsmnd.mthr@gmail.com>
2019-11-02 19:24:55 +0100

    added env variable for secret key

--- mysite/mysite/settings/dev.py
+++ mysite/mysite/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'removed' #environment variable — see below
+SECRET_KEY = 'removed' #environment variable, see below
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['rosamund.pythonanywhere.com']
