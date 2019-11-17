6172d63d2483da25d7f40b93e0f2c92ae0041831
rosamund <rsmnd.mthr@gmail.com>
2019-11-17 11:43:14 +0100

    modified local wsgi file

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -12,6 +12,6 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my-wagtail-blog.mysite.settings.dev")
 
 application = get_wsgi_application()
